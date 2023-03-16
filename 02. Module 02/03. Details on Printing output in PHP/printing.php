eb122385e36a94101dc26359c6586707df28ff3e
Somel Ahmed <somelahmed88@gmail.com>
2023-03-16 11:18:19 +0600

    sprintf added

---
+++ 02. Module 02/03. Details on Printing output in PHP/printing.php
@@ -0,0 +1,47 @@
+<?php
+//! Data types
+/*
+integer int
+Double / float
+Boolean
+Null
+Array
+Object
+Resource
+*/
+// $result = true;
+// var_dump($result);
+
+// $name = "Earth";
+// echo "We're living on $name";
+// echo "We're living on {$name}";
+// echo 'We\'re living on '.$name;
+
+// $upperName = strtoupper($name);
+// echo "We're living on {$upperName}";
+// printf("We're living on %s", $name);
+
+// $fname = "Isaac";
+// $lname = "Newton";
+// printf("His %s name is %s %s","Full", $fname, $lname);
+
+// echo " His
+// Name
+// is 
+// {$fname} {$lname}\n
+// , How Are you?
+// ";
+
+
+
+$planet1 = "Mercury";
+$planet2 = "Jupiter";
+echo "The smallest planet is ".$planet1. " and the biggest planet is ".$planet2."\n";
+
+echo "The smallest planet is {$planet1}  and the biggest planet is {$planet2}\n";
+
+printf("The Biggest planet is %s and the Smallest Planet is %s\n",strtoupper($planet1),strtoupper( $planet2));
+
+
+
+
